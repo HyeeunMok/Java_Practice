3572c27229164cc5ab39197364cd8dca49ced784
Hyeeun Mok <41407354+HyeeunMok@users.noreply.github.com>
2018-08-15 10:02:57 -0400

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,31 @@
+package vienSClass;
+
+import java.util.Scanner;
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		
+		// Create an array of strings.
+		String[] names = new String[5];
+		
+		// Define a scanner to read input.
+		Scanner scan = new Scanner(System.in);
+		
+		//Read in the names and store them in the array of strings.
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Please enter a name.");
+			names[i] = scan.nextLine();
+		}
+		
+		// Output the list of names
+		System.out.println("The names entered were:");
+		for (int i = 0; i < 5; i++) {
+			System.out.println(names[i]);
+		}
+		
+		scan.close();
+
+	}
+
+}
