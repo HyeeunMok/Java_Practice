a0f6c71484359c7a8cf1ad459c6e2119221740e3
Hyeeun Mok <41407354+HyeeunMok@users.noreply.github.com>
2018-08-20 07:48:29 -0400

    Add files via upload

---
+++ oOP1_Car/Test_Car.java
@@ -0,0 +1,15 @@
+package oOP1_Car;
+
+public class Test_Car {
+
+	public static void main(String[] args) {
+		
+		Tesla tesla = new Tesla(36);
+		tesla.steer(45);
+		tesla.accelerate(30);
+		tesla.accelerate(20);
+		tesla.accelerate(-42);
+
+	}
+
+}
